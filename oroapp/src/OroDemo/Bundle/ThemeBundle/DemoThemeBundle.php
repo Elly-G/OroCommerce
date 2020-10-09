53c3963f06813e89509e0c9c7bf54050e66660ee
Elly-G <lenuleag@gmail.com>
2020-10-09 12:13:25 +0300

    add stable version with product attribute in products page

---
+++ oroapp/src/OroDemo/Bundle/ThemeBundle/DemoThemeBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace OroDemo\Bundle\ThemeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DemoThemeBundle extends Bundle
+{
+
+}
--- oroapp/src/Tara/Bundle/CustomThemeBundle/TaraCustomThemeBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tara\Bundle\CustomThemeBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class TaraCustomThemeBundle extends Bundle
-{
-}
